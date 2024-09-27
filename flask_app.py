d0001455844534a7879bb3529ea882c10ef4d293
Gary Dohmeier II <gdohmeier@gmail.com>
2024-09-27 21:41:32 +0000

    added template dir and main html file

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,20 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
+
+@app.route('/hello')
 def hello_world():
-    return ' .... Hello from Flask!'
+    return '.... Hello from Flask!'
 
 @app.route('/foo')
-def hello_world2():
-    return ' Hello, this is the foo page.'
+def page_foo():
+    return 'Hello, this is the foo page.'
+
+@app.route("/")
+def index():
+    return render_template("main_page.html")
